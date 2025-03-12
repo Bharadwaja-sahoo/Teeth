56e6e7dff7f07b80a2cfc56ed06510ba003ea9c8
Bharadwaja sahoo <bharadwajasahoo96@gmail.com>
2025-03-12 11:33:14 +0530

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,169 @@
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.main',
+        pin: true,
+        start: '50% 50%',
+        end: '200% 50%',
+        scrub: true,
+        // markers: true,
+    }
+});
+tl.to("#sev", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.3
+})
+    .to("#tfive", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#two", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#fift", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#tnin", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#ten", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#thione", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#thr", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#tsev", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#thi", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#eig", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#nint", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#fiv", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#sixt", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#one", {
+        opacity: 1,
+        filter: "blur(0px)"
+    })
+    .to("#thirtwo", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#twont", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#tfor", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#twl", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#teig", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#nin", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#ttwo", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#tone", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#thirty", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#six", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#ele", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#for", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#tthre", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#eight", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#sevent", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#fort", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
+    .to("#tsix", {
+        opacity: 1,
+        filter: "blur(0px)",
+        delay: -0.3
+    })
